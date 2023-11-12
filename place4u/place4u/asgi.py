df3d77c17c5f06231e2e47de058465f041458e3d
hjcho <unlimit13@naver.com>
2023-11-12 19:59:15 +0900

    Web demo init

---
+++ place4u/place4u/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for place4u project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'place4u.settings')
+
+application = get_asgi_application()
