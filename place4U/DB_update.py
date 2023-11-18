4ecb501f6563e88936083deef81036da74e7a7a1
Hyungjun Cho <74866465+unlimit13@users.noreply.github.com>
2023-11-19 03:27:19 +0900

    Update DB_update.py

--- place4U/DB_update.py
+++ place4U/DB_update.py
@@ -5,6 +5,9 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 from django.utils import timezone
 
+def gptcall(caption):
+    place = 0#chatgpt return
+    return place
 
 def main():
     from SearchAndList.models import searchedList,searchedTag
@@ -14,7 +17,9 @@ def main():
         print(line)
         new_tag = searchedTag(searchedTag_text="TEST", location="동탄",pub_date=timezone.now())
         new_tag.save()
-        print(new_tag.id)
+        #print(new_tag.id)
+        tag = searchedTag.objects.get(id=new_tag.id)
+        tag.searchedlist_set.create(caption=line.caption,likes=line.like_count,place=gptcall())
     
 if __name__=="__main__":
     main()
