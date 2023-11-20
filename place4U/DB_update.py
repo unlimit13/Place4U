003f055bfa8e725bee6dd9ecdb0171d4b836faaa
hjcho <unlimit13@naver.com>
2023-11-20 14:04:10 +0900

    내 위치 사용 -> 한글주소

--- place4U/DB_update.py
+++ place4U/DB_update.py
@@ -10,11 +10,12 @@ def main():
     from SearchAndList.models import searchedList,searchedTag
     file = open("crawling_data.csv",'r')
     reader = csv.reader(file)
+    new_tag = searchedTag(searchedTag_text="TEST", location="동탄",pub_date=timezone.now())
+    new_tag.save()
     for line in reader:
         print(line)
-        new_tag = searchedTag(searchedTag_text="TEST", location="동탄",pub_date=timezone.now())
-        new_tag.save()
         print(new_tag.id)
+        new_tag.searchedlist_set.create(caption=line,likes=line,place=line)
     
 if __name__=="__main__":
     main()
