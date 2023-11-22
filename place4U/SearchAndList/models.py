9c36c28bf81cb0b34de204e425ea541f9c2c23be
hjcho <unlimit13@naver.com>
2023-11-22 19:26:37 +0900

    DB modify : input location-input tag set as key

--- place4U/SearchAndList/models.py
+++ place4U/SearchAndList/models.py
@@ -5,17 +5,21 @@ import datetime
 class searchedTag(models.Model):
     searchedTag_text = models.CharField(max_length=200)
     searchedLocation_text = models.CharField(max_length=100)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['searchedTag_text', 'searchedLocation_text'], name='unique_set'),
+        ]
     pub_date = models.DateTimeField("date published")
     def __str__(self) -> str:
-        return str(self.searchedTag_text)
+        return f"{self.searchedTag_text} - {self.searchedLocation_text}"
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 # Create your models here.
 
 class searchedList(models.Model):
-    Tag = models.ForeignKey(searchedTag,on_delete=models.CASCADE)
+    tag_location = models.ForeignKey(searchedTag, on_delete=models.CASCADE, related_name='searched_lists')
     caption = models.CharField(max_length=3000)
     likes = models.IntegerField(default=0)
     place = models.CharField(max_length=200)
     def __str__(self) -> str:
-        return str(self.Tag)
+        return str(self.tag_location)
